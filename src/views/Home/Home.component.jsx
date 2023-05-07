6fcdbcb3e0d4f73712c0de27886f2d7c508729bd
Dhruv <55016342+Dhruv261@users.noreply.github.com>
2023-05-07 22:00:47 +0530

    Update Home.component.jsx

--- src/views/Home/Home.component.jsx
+++ src/views/Home/Home.component.jsx
@@ -3,26 +3,6 @@ import Cryptic from '../../components/CrypticTab/Cryptic.component';
 
 
 const Home = () => {
-  // const { userLogged, movieData } = useContext(UserContext);
-
-  // const [passList, setPassList] = useState("");
-
-  // useEffect(()=> {
-  //   fetch('http://localhost:3001/pass-list', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       token: 'Bearer' + sessionStorage.getItem('token'),
-  //     },
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       // console.log(data);
-  //       setPassList(data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, [])
-
   return (
     <div>
       <Navigation />
